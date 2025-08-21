a6cbb0f3a74aa6e871cdb3e31bae7e4eb1e4d9f8
Jakob Stender Guldberg <jakob1379gmail.com>
2025-08-21 10:56:38 +0200

    build: Update pre-commit configuration and Nix flake dependencies

--- flake.nix
+++ flake.nix
@@ -4,13 +4,19 @@
   };
   outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
     let
-      pkgs = nixpkgs.legacyPackages.system;
+      pkgs = nixpkgs.legacyPackages.${system};
     in
       {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             uv
+            cacert
           ];
+
+          shellHook = ''
+            export UV_PYTHON_PREFERENCE="managed"
+            export SSL_CERT_FILE=$(nix eval --raw nixpkgs.cacert)/etc/ssl/certs/ca-bundle.crt
+            '';
         };
       }
   );
